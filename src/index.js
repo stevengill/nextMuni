477e03548df351ebf96f80747c9f383640ff44fc
snopeks <stephaniesnopek@gmail.com>
2017-02-15 21:55:16 -0800

    big update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const alexa = require('alexa-app');
 const fetch = require('node-fetch');
 const app = new alexa.app('sample');
@@ -37,30 +39,30 @@ app.intent('nextBus',
     let url = "http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&stopId="
     if(busesJson[bus]) {
         getSchedule = true;
-        url += buses[bus]['stopID'];
+        url += busesJson[bus]['stopID'];
     } else {
-        //send a response saying we didn't understand the request   
+        //send a response saying we didn't understand the request
         console.log('bus: ' + bus + ' was not in buses list');
         response.fail('Your bus is not supported');
     }
     console.log(bus);
     console.log(typeof bus)
-        
+
     if(getSchedule) {
         console.log(url);
         fetch(url)
         .then(function(res) {
-            //send result to back to echo 
+            //send result to back to echo
             response.say(handleResult(res.text(), bus)).send();
         });
     };
-    // Return false immediately so alexa-app doesn't send the response 
-    return false; 
+    // Return false immediately so alexa-app doesn't send the response
+    return false;
   }
 );
 app.sessionEnded(function(request,response) {
     // No response necessary
-    console.log('session end'); 
+    console.log('session end');
 });
 
 exports.handler = app.lambda();
